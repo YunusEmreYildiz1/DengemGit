b5a0b23344b64a2700b02f51e33f99277e853d62
YunusEmreYildiz0 <154478431+YunusEmreYildiz1@users.noreply.github.com>
2024-08-28 02:20:37 +0300

    tüm görevler kısmınında bottomNavigationbar düzenlendi

--- lib/tumunu_gor_gorevler.dart
+++ lib/tumunu_gor_gorevler.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class Task {
   final String title;
@@ -25,6 +26,21 @@ class TumGorev extends StatelessWidget {
     Task(
       title: 'Arkadaş Edinme',
       subtitle: 'Okuma Süresi: 10dk',
+      imagePath: 'assets/images/gorsel7.JPG',
+    ),
+    Task(
+      title: 'Sohbet etme',
+      subtitle: 'Okuma Süresi: 4dk',
+      imagePath: 'assets/images/gorsel7.JPG',
+    ),
+    Task(
+      title: 'Sohbet etme',
+      subtitle: 'Okuma Süresi: 4dk',
+      imagePath: 'assets/images/gorsel7.JPG',
+    ),
+    Task(
+      title: 'Sohbet etme',
+      subtitle: 'Okuma Süresi: 4dk',
       imagePath: 'assets/images/gorsel7.JPG',
     ),
     Task(
@@ -122,45 +138,19 @@ class TumGorev extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 10,
-              offset: Offset(0, -2),
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          currentIndex: 0,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Color(0xFF8B70FC),
-          unselectedItemColor: Colors.grey,
-          selectedLabelStyle: GoogleFonts.poppins(fontSize: 14),
-          unselectedLabelStyle: GoogleFonts.poppins(fontSize: 14),
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home,
-              size: 28.0,),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.music_note,size: 28.0,),
-              label: 'Music',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.self_improvement,size: 30.0,),
-              label: 'Yanındayım',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person,size: 28.0,),
-              label: 'Profilim',
-            ),
-          ],
-        ),
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.white,
+        height: 80,
+        elevation: 0,
+        selectedIndex: 0,  //hangisi seçili olduğu gösteriyo her sayfa için mutlaka bunu düzenle
+
+
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home,size: 28.0,), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.music_note,size: 28.0,), label: 'Music'),
+          NavigationDestination(icon: Icon(Icons.chat,size: 30.0,), label: 'Yanındayım',),
+          NavigationDestination(icon: Icon(Icons.person,size: 28.0,), label: 'Profilim'),
+        ],
       ),
     );
   }
