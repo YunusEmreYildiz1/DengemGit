eff0a8710e34fd763f69bf8d19046e34d4ea39c0
YunusEmreYildiz0 <154478431+YunusEmreYildiz1@users.noreply.github.com>
2024-09-04 11:53:50 +0300

    info sayfası düzeltildi

--- lib/info.dart
+++ lib/info.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Info extends StatefulWidget {
-  final String title;
-  final String description;
-  final String imagePath;
-
-  const Info(this.title, this.description, this.imagePath, {Key? key}) : super(key: key);
+  const Info({Key? key}) : super(key: key);
 
   @override
   State<Info> createState() => _InfoState();
 }
 
 class _InfoState extends State<Info> {
-  late String _title;
-  late String _description;
-  late String _imagePath;
-  final DraggableScrollableController _scrollableController =
-  DraggableScrollableController();
+  final DraggableScrollableController _scrollableController = DraggableScrollableController();
   bool _isFullyOpened = false;
+
+  String _title = "Sosyal Anksiyete Nedir?";
+  String _description = "Sosyal fobi olarak da bilinir. Bireyin başkaları tarafından yargılanma kaygısını taşıdığı toplumsal ortamlarda mahcup ya da rezil olacağı konusunda belirgin ve sürekli korkusunun olduğu bir kaygı bozukluğudur.";
+  String _hakkinda = "Hakkında";
+  final String _imagePath = "assets/images/anskeyete.png";
+
+  String _initialDescription = "Sosyal fobi olarak da bilinir. Bireyin başkaları tarafından yargılanma kaygısını taşıdığı toplumsal ortamlarda mahcup ya da rezil olacağı konusunda belirgin ve sürekli korkusunun olduğu bir kaygı bozukluğudur.";
 
   @override
   void initState() {
     super.initState();
-
-    _title = widget.title;
-    _description = widget.description;
-    _imagePath = widget.imagePath;
 
     _scrollableController.addListener(() {
       setState(() {
@@ -49,20 +44,42 @@ class _InfoState extends State<Info> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                ClipRect(
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    heightFactor: .9,
-                    widthFactor: 1,
-                    child: Transform.scale(
-                      scale: 1.65,
-                      child: Image.asset(
-                        _imagePath,
-                        width: 500,
-                        height: 400,
+                Stack(
+                  children: [
+                    ClipRect(
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        heightFactor: .9,
+                        widthFactor: 1,
+                        child: Transform.scale(
+                          scale: 1.65,
+                          child: Image.asset(
+                            _imagePath,
+                            width: 500,
+                            height: 400,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    Positioned(
+                      top: 40,
+                      left: 15,
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFF1B1E28).withOpacity(0.3),
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.arrow_back_ios_new, color: Colors.white,size: 17,),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -121,17 +138,29 @@ class _InfoState extends State<Info> {
                                   const Spacer(),
                                   IconButton(
                                     onPressed: () {
-                                      // Add your logic here
+                                      setState(() {
+                                        if (_description == _initialDescription) {
+                                          _description = "citation";
+                                        } else {
+                                          _description = _initialDescription;
+                                        }
+
+                                        if (_hakkinda == "Hakkında") {
+                                          _hakkinda = "Akademik Dayanağı";
+                                        } else {
+                                          _hakkinda = "Hakkında";
+                                        }
+                                      });
                                     },
                                     icon: const Icon(Icons.info),
                                   ),
                                 ],
                               ),
                               const SizedBox(height: 40),
-                              const Text(
-                                'Hakkında',
+                              Text(
+                                _hakkinda,
                                 style: TextStyle(
-                                  fontSize: 24,
+                                  fontSize: 22,
                                   color: Colors.black,
                                 ),
                                 overflow: TextOverflow.ellipsis,
--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,20 @@ import 'package:untitled6/introduction.dart';
 import 'package:untitled6/loadingscreen.dart';
 import 'package:untitled6/breathe.dart';
 import 'package:untitled6/profil_sayfa.dart';
-import 'package:untitled6/info.dart';
+import 'package:untitled6/info.dart';  // Info sayfasını içe aktarıyoruz
 import 'package:untitled6/info2.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Info("Sosyal Anksiyete Nedir?", "Sosyal fobi olarak da bilinir. Bireyin başkaları tarafından yargılanma kaygısını taşıdığı toplumsal ortamlarda mahcup ya da rezil olacağı konusunda belirgin ve sürekli korkusunun olduğu bir kaygı bozukluğudur.", "assets/images/anskeyete.png"),
+    home: Info(),  // Info sayfasını ana sayfa olarak belirliyoruz
   ));
 }
 
 class AnaUygulama extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: profil(),
     );
   }
-
 }
